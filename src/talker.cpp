6054fa00b1b846159e13d005dc4e841fd343010b
kushagra7176 <kushagraagrawal425@gmail.com>
2020-10-26 22:44:01 -0400

    Modified talker.cpp

--- src/talker.cpp
+++ src/talker.cpp
@@ -65,7 +65,7 @@ int main(int argc, char **argv) {
     std_msgs::String msg;
 
     std::stringstream ss;
-    ss << "Hello World!! " << count;
+    ss << "I am Kushagra Agrawal. I love robots!! " << count;
     msg.data = ss.str();
 
     ROS_INFO("%s", msg.data.c_str());
